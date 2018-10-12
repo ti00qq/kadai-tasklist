8d583fa4790a9eab2f91286b53e04f79d3c1aff9
ti00qq <ti00qq@gmail.com>
2018-10-12 15:08:03 +0000

    CRUD messages

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>タスク一覧</h1>
+
+    @foreach ($tasks as $task)
+        <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+    @endforeach
+    {!! link_to_route('tasks.create', '新規タスクの作成') !!}
+@endsection
