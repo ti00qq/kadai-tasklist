995b6d1760caef63dbe486ecd2626a54c3ac43e2
ti00qq <ti00qq@gmail.com>
2018-10-15 14:49:18 +0000

    add to offset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         @include('commons.navbar')
 
         <div class="container">
-            <div class="col-xs-12 col-sm-8 col-lg-6">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
                 @include('commons.error_messages')
     
                 @yield('content')
